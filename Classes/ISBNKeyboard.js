80d2812b23c59360105db92c9bf77c8a3c18d050
Rinat <rinat@tuslisoftware.com>
2017-10-05 15:39:56 -0600

    design custom keyboard

--- Classes/ISBNKeyboard.js
+++ Classes/ISBNKeyboard.js
@@ -4,8 +4,14 @@ import {
   Text,
   View,
   Modal,
+  StyleSheet,
+  Dimensions,
 } from 'react-native';
 import { register, insertText, backSpace,submit } from 'react-native-custom-keyboard';
+import FontAwesome, { Icons } from 'react-native-fontawesome'
+
+let screenHeight = Dimensions.get('window').height*0.98;
+let screenWidth = Dimensions.get('window').width;
 
 class MyKeyboard extends Component {
 
@@ -15,8 +21,6 @@ class MyKeyboard extends Component {
     }
     else if(value=='submit'){
         insertText(this.props.tag, '\n');
-
-
     }
     else{
         insertText(this.props.tag, value);
@@ -24,64 +28,81 @@ class MyKeyboard extends Component {
   };
   render() {
     return (
-<View>
-
+    <View style={{backgroundColor:'rgb(236,239,241)', height:screenHeight}}>
         <View style={{flexDirection:'row'}}>
-            <TouchableOpacity onPress={this.onPress.bind(this,"1")}>
-              <Text style={{color:'black'}}>1</Text>
+            <View style={styles.keyBoardColumnStyle}>
+                <TouchableOpacity onPress={this.onPress.bind(this,"submit")}>
+                  <Text style={styles.keyBoardKeyStyle}>Search</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.onPress.bind(this,"1")}>
+                  <Text style={styles.keyBoardKeyStyle}>1</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.onPress.bind(this,"4")}>
+                  <Text style={styles.keyBoardKeyStyle}>4</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.onPress.bind(this,"7")}>
+                  <Text style={styles.keyBoardKeyStyle}>7</Text>
+                </TouchableOpacity>
+            </View>
+        <View style={styles.keyBoardColumnStyle}>
+            <TouchableOpacity onPress={this.onPress.bind(this,"bs")}>
+                <FontAwesome style={styles.keyBoardKeyStyle}>{Icons.eraser}</FontAwesome>
             </TouchableOpacity>
             <TouchableOpacity onPress={this.onPress.bind(this,"2")}>
-              <Text style={{color:'black'}}>2</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={this.onPress.bind(this,"3")}>
-              <Text style={{color:'black'}}>3</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={{flexDirection:'row'}}>
-            <TouchableOpacity onPress={this.onPress.bind(this,"4")}>
-              <Text style={{color:'black'}}>4</Text>
+              <Text style={styles.keyBoardKeyStyle}>2</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={this.onPress.bind(this,"5")}>
-              <Text style={{color:'black'}}>5</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={this.onPress.bind(this,"6")}>
-              <Text style={{color:'black'}}>6</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={{flexDirection:'row'}}>
-            <TouchableOpacity onPress={this.onPress.bind(this,"7")}>
-              <Text style={{color:'black'}}>7</Text>
+              <Text style={styles.keyBoardKeyStyle}>5</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={this.onPress.bind(this,"8")}>
-              <Text style={{color:'black'}}>8</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={this.onPress.bind(this,"9")}>
-              <Text style={{color:'black'}}>9</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={{flexDirection:'row'}}>
-            <TouchableOpacity onPress={this.onPress.bind(this,"X")}>
-              <Text style={{color:'black'}}>X</Text>
+              <Text style={styles.keyBoardKeyStyle}>8</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={this.onPress.bind(this,"0")}>
-              <Text style={{color:'black'}}>0</Text>
+              <Text style={styles.keyBoardKeyStyle}>0</Text>
+            </TouchableOpacity>
+        </View>
+        <View style={styles.keyBoardColumnStyle}>
+            <TouchableOpacity onPress={this.onPress.bind(this,"X")}>
+              <Text style={styles.keyBoardKeyStyle}>X</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.onPress.bind(this,"3")}>
+              <Text style={styles.keyBoardKeyStyle}>3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.onPress.bind(this,"6")}>
+              <Text style={styles.keyBoardKeyStyle}>6</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.onPress.bind(this,"9")}>
+              <Text style={styles.keyBoardKeyStyle}>9</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={this.onPress.bind(this,"978")}>
-              <Text style={{color:'black'}}>978</Text>
+              <Text style={styles.keyBoardKeyStyle}>978</Text>
             </TouchableOpacity>
         </View>
-        <View style={{flexDirection:'row'}}>
-            <TouchableOpacity onPress={this.onPress.bind(this,"submit")}>
-              <Text style={{color:'black'}}>Search</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={this.onPress.bind(this,"bs")}>
-              <Text style={{color:'black'}}>backspace</Text>
-            </TouchableOpacity>
-        </View>
-      </View>
+    </View>
 
-    );
-  }
+  </View>
+
+  );
+ }
 }
+
+const styles = StyleSheet.create({
+  keyBoardColumnStyle:{
+    justifyContent:'flex-start',
+    alignItems:'flex-end',
+    padding:7,
+
+  },
+  keyBoardKeyStyle:{
+    color:'black',
+    fontSize:20,
+    height:screenHeight*0.06,
+    width:screenWidth*0.33,
+    alignSelf:'center',
+    paddingLeft:35
+
+
+  },
+ });
 
 register('hello', () => MyKeyboard);
